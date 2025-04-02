078c598934f4e87e4938ca3b02f9548800050d31
eveykong <elkongadzem@bremer.com>
2025-04-02 15:55:05 -0500

    update the sql notebook to contain multiple cells

--- fis-bic-load-to-bronze-table.sql
+++ fis-bic-load-to-bronze-table.sql
@@ -1,12 +1,25 @@
 -- Databricks notebook source
-           CREATE TABLE databricks_workspace_sandbox_03.tf_raw_ingest.ok_tb_cc_oz4_b2_acct_ard (
-      bnk_nbr varchar(65535),
+-- MAGIC %md
+# Move data frim redshift to databricks
+
+-- COMMAND ----------
+-- MAGIC %sql
+-- Create tf_raw_ingest.ok_tb_cc_oz4_b2_acct_ard table in databricks raw ingest schema
+CREATE TABLE databricks_workspace_sandbox_03.tf_raw_ingest.ok_tb_cc_oz4_b2_acct_ard (
+    bnk_nbr varchar(65535),
       prcs_dte date
       )
       USING PARQUET
          PARTITIONED BY (prcs_dte)
          LOCATION 's3://raw-data-sandbox-813140711/fis_bic/ok/tb_cc_oz4_b2_acct_ard';
-
-      CREATE OR REPLACE TABLE databricks_workspace_sandbox_03.tf_warehouse_staging.ok_tb_cc_oz4_b2_acct_ard CLONE databricks_workspace_sandbox_03.tf_raw_ingest.ok_tb_cc_oz4_b2_acct_ard;
+    
+-- COMMAND ----------
+-- MAGIC %sql
+-- Copy databricks raw ingest table to databricks tf_warehouse_staging schema
       
-      DROP TABLE databricks_workspace_sandbox_03.tf_raw_ingest.ok_tb_cc_oz4_b2_acct_ard;
+CREATE OR REPLACE TABLE databricks_workspace_sandbox_03.tf_warehouse_staging.ok_tb_cc_oz4_b2_acct_ard CLONE databricks_workspace_sandbox_03.tf_raw_ingest.ok_tb_cc_oz4_b2_acct_ard;
+
+-- COMMAND ----------
+-- MAGIC %sql
+-- Drop the raw_ingest table from databricks   
+DROP TABLE databricks_workspace_sandbox_03.tf_raw_ingest.ok_tb_cc_oz4_b2_acct_ard;
